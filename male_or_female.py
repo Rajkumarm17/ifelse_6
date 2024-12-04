e7c9bb4c63bee5fb6ce32e4acefa9a23b8e32076
Rajkumar17 <rajkumarm1703@gmail.com>
2024-12-04 19:28:23 +0530

    male or female

---
+++ male_or_female.py
@@ -0,0 +1,8 @@
+# 6 check user is male or female
+n = input("enter a gender: ")
+if n =="male" or n == "Male":
+    print("user is male")
+elif n == "female" or n == "Female":
+    print("user is female")
+else:
+    print("please enter valid gender")
